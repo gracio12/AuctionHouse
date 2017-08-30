f33d1f7837b4219e310e5ead7508af13796079f3
gracio12 <gracio12@gmail.com>
2017-08-30 14:19:46 +0200

    lol2

--- src/main/java/xd/auctionhouse/Controller/AndroidController.java
+++ src/main/java/xd/auctionhouse/Controller/AndroidController.java
@@ -6,10 +6,7 @@ import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import xd.auctionhouse.Entity.User;
 
 import xd.auctionhouse.Service.UserService;
@@ -29,7 +26,7 @@ public class AndroidController {
         this.userService = userService;
     }
 
-    @PostMapping("/login")
+    @GetMapping("/login")
     @ResponseBody
     public String logowanie(String haslo, String email) throws JSONException {
 
